9a13b9cb4f0ff7a173abaaeba6a3622d7c886356
Rafael Morel LDS <rafael@latchcorp.com>
2026-07-23 21:37:05 -0400

    Protect CloseFlow with login

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import AuthApp from './AuthApp'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthApp>
+      <App />
+    </AuthApp>
   </StrictMode>,
 )
